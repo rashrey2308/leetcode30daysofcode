ad51ce45cc86f9690a5ff473f708030b5f68dac2
Rasika Kedia <51507201+rashrey2308@users.noreply.github.com>
2020-07-13 16:41:50 +0530

    Create prisoncells.cpp
    Code for the problem Prison Cells After N Days

---
+++ prisoncells.cpp
@@ -0,0 +1,48 @@
+/*There are 8 prison cells in a row, and each cell is either occupied or vacant.
+Each day, whether the cell is occupied or vacant changes according to the following rules:
+If a cell has two adjacent neighbors that are both occupied or both vacant, then the cell becomes occupied.
+Otherwise, it becomes vacant.
+(Note that because the prison is a row, the first and the last cells in the row can't have two adjacent neighbors.)
+We describe the current state of the prison in the following way: cells[i] == 1 if the i-th cell is occupied, else cells[i] == 0.
+Given the initial state of the prison, return the state of the prison after N days (and N such changes described above.)
+
+Example:
+
+Input: cells = [0,1,0,1,1,0,0,1], N = 7
+Output: [0,0,1,1,0,0,0,0]
+Explanation: 
+The following table summarizes the state of the prison on each day:
+Day 0: [0, 1, 0, 1, 1, 0, 0, 1]
+Day 1: [0, 1, 1, 0, 0, 0, 0, 0]
+Day 2: [0, 0, 0, 0, 1, 1, 1, 0]
+Day 3: [0, 1, 1, 0, 0, 1, 0, 0]
+Day 4: [0, 0, 0, 0, 0, 1, 0, 0]
+Day 5: [0, 1, 1, 1, 0, 1, 0, 0]
+Day 6: [0, 0, 1, 0, 1, 1, 0, 0]
+Day 7: [0, 0, 1, 1, 0, 0, 0, 0]
+*/
+
+
+class Solution {
+public:
+    vector<int> prisonAfterNDays(vector<int>& cells, int N) {
+        vector<int> prev;
+        int n= N%14==0?14 : N%14;
+        for(int i=0;i<n;i++)
+        {
+            prev=cells;
+            for(int j=0;j<8;j++)
+            {
+                if(j==0)
+                    cells[0]=0;
+                else if(j==7)
+                    cells[7]=0;
+                else if (prev[j-1]==prev[j+1])
+                    cells[j]=1;
+                else
+                    cells[j]=0;
+            }
+        }
+        return cells;
+    }
+};
